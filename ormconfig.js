3201f1e8bc64c7b801a7469993b8fa99cef6781f
Pedro Dalla Bernardina <l.pedro.nascimento@gmail.com>
2021-08-04 11:58:12 -0300

    First commit

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,10 @@
-{
-  "username": "postgres",
-  "password": "docker",
-
-  "type": "postgres",
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   "name": "default",
   "host": "localhost",
   "port": 5432,
